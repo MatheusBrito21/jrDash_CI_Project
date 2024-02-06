b50eabe51b061b0bd82aa1b58541cad4529a8a59
Matheus Brito Biserra <matheus.biserra@globalhitss.com.br>
2024-02-06 15:59:56 -0300

    projeto iniciado

---
+++ application/views/inc/header_view.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" href="<?=base_url()?>public/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="<?=base_url()?>public/css/syle.css" />
+
+	<script src="<?=base_url()?>public/js/jquery.js"></script>
+	<script src="<?=base_url()?>public/js/bootstrap.js"></script>
+
+	<title>jrDash</title>
+</head>
+<body>
+	<header>
+		jrDash
+	</header>
+
